2e2cb0a687e123fcf361eb6e9e8521b935c02297
MITCH\Mitch <mitchdoran92@gmail.com>
2020-05-26 23:16:38 +1000

    Setup OnPress and OnRelease for keys to control npc.direction

--- content/dota_addons/boss_battles/panorama/scripts/custom_game/custom_hotkeys_players.js
+++ content/dota_addons/boss_battles/panorama/scripts/custom_game/custom_hotkeys_players.js
@@ -1,5 +1,12 @@
 "use strict";
 
+function WrapFunction(name) {
+    return function() {
+        Game[name]();
+    };
+}
+Game.EmptyCallback = function() {};
+
 function MyCallBack(){
     //$.Msg("your awesome message")
     //Abilities.ExecuteAbility( integer nAbilityEntIndex, integer nCasterEntIndex, boolean bIsQuickCast )
@@ -24,34 +31,84 @@ function MyCallBack(){
 
 }
 
-function EmptyCallBack(){
-
-}
 //order, direction
 function Move(){
-
     var playerId = Players.GetLocalPlayer();
     var heroIndex = Players.GetPlayerHeroEntityIndex(playerId);
     
-    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "up" });
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "up", });
     //GameEvents.SendCustomGameEventToServer(order, { entityIndex: heroIndex, direction: direction });
     $.Msg("Sent game event...")
 }
 
-function MoveUnit(){
 
+function OnPressW(){
+    $.Msg("Javascript: OnPressW()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "up", keyPressed: "w", keyState: "down" });
+}
+
+function OnReleaseW(){
+    $.Msg("Javascript: OnReleaseW()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "up", keyPressed: "w",  keyState: "up" });
+}
+
+function OnPressD() {
+    $.Msg("Javascript: OnPressD()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "right", keyPressed: "d",  keyState: "down" });
+}
+
+function OnReleaseD() {
+    $.Msg("Javascript: OnReleaseD()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "right", keyPressed: "d",  keyState: "up" });
+}
+
+function OnPressS() {
+    $.Msg("Javascript: OnPressS()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "down", keyPressed: "s",  keyState: "down" });
+}
+
+function OnReleaseS() {
+    $.Msg("Javascript: OnReleaseS()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "down", keyPressed: "s",  keyState: "up" });
+}
+
+function OnPressA() {
+    $.Msg("Javascript: OnPressA()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "left", keyPressed: "a",  keyState: "down" });
+}
+
+function OnReleaseA() {
+    $.Msg("Javascript: OnReleaseA()...")
+    var heroIndex = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    GameEvents.SendCustomGameEventToServer("MoveUnit", { entityIndex: heroIndex, direction: "", keyPressed: "a",  keyState: "up" });
 }
 
 (function()
 {
-    Game.AddCommand( "+S", MyCallBack, "", 0 );
-    Game.AddCommand( "-S", EmptyCallBack, "", 0 );
+    //Tried to use WrapFunction but it doesn't work for me. No idea what it does.
+    // Game.AddCommand( "+W", WrapFunction("OnPressW"), "", 0 );
+    // Game.AddCommand( "-W", WrapFunction("OnReleaseW"), "", 0 );    
 
-    Game.AddCommand( "+W", Move, "", 0 );
-    Game.AddCommand( "-W", EmptyCallBack, "", 0 );
+    //W and S work.
+    //A and D do not work. Probably because they have other bindings for me, A = attack, D = ability
+    Game.AddCommand( "+W", OnPressW, "", 0 );
+    Game.AddCommand( "-W", OnReleaseW, "", 0 );    
 
-    //Game.OnPressW =     function(){ Move( "MoveUnit", "up" ) }
-    //Game.OnReleaseW =   function(){ Move( "StopUnit", "up" ) }
+    Game.AddCommand( "+D", OnPressD, "", 0 );
+    Game.AddCommand( "-D", OnReleaseD, "", 0 );    
+
+    Game.AddCommand( "+S", OnPressS, "", 0 );
+    Game.AddCommand( "-S", OnReleaseS, "", 0 );    
+
+    Game.AddCommand( "+A", OnPressA, "", 0 );
+    Game.AddCommand( "-A", OnReleaseA, "", 0 );    
 
 })();
 
--- game/dota_addons/boss_battles/scripts/vscripts/managers/player_manager.lua
+++ game/dota_addons/boss_battles/scripts/vscripts/managers/player_manager.lua
@@ -1,23 +1,49 @@
 function GameMode:SetUpMovement()
     CustomGameEventManager:RegisterListener('MoveUnit', function(eventSourceIndex, args)
-        --local direction = args.direction
-
+        local direction = args.direction
+        local keyPressed = args.keyPressed
+        local keyState = args.keyState
+        --DEBUG: print all args passed in
         --for k, v in pairs(args) do
             --print("k = ",k,"v = ",v)
         --end
 
         local unit = EntIndexToHScript(args.entityIndex)
-
         if unit == nil then return end
-        if unit.direction == nil then return end--then
-            --unit.direction = {
-             --   y = 0,
-             --   x = 0
-            --}
-      --  end
+        --if unit.direction == nil then return end
 
-        --print(unit:GetUnitName())
-        --print(unit.direction.y)
+        -- W key
+        if keyPressed == "w" and keyState == "down" then
+            unit.direction.y = 1
+        end
+        if keyPressed == "w" and keyState == "up" then
+            unit.direction.y = 0
+        end
+
+        -- D key
+        if keyPressed == "d" and keyState == "down" then
+            unit.direction.x = 1
+        end
+        if keyPressed == "d" and keyState == "up" then
+            unit.direction.x = 0
+        end
+
+        -- S key
+        if keyPressed == "s" and keyState == "down" then
+            unit.direction.y = -1
+        end
+        if keyPressed == "s" and keyState == "up" then
+            unit.direction.y = 0
+        end
+
+        -- A key
+        if keyPressed == "a" and keyState == "down" then
+            unit.direction.x = -1
+        end
+        if keyPressed == "a" and keyState == "up" then
+            unit.direction.x = 0
+        end
+
 
         if args.direction == "up" then
             --local currentLocation = unit:GetAbsOrigin()
@@ -31,24 +57,10 @@ function GameMode:SetUpMovement()
             --print("Moving hero forward")
             --print("before movement command Unit.direction.y = ", unit.direction.y)
            
-            unit.direction.y = unit.direction.y + 1
+            --unit.direction.y = unit.direction.y + 1
+            --unit.direction.y = 1
             --print("after movement command Unit.direction.y = ", unit.direction.y)
         end
-
-    end)
-
-    CustomGameEventManager:RegisterListener('StopUnit', function(eventSourceIndex, args)
-
-        local unit = EntIndexToHScript(args.entityIndex)
-
-        if unit == nil then return end
-        if unit.direction == nil then return end
-
-        if args.direction == "down" then
-            unit.direction.y = unit.direction.y - 1
-        end
-
-    end)
-
+    end) -- end of MoveUnit listener
 
 end
