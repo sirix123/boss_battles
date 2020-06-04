fb7c0005315a00cb5a94142bc5dd8bcedba8d975
MITCH\Mitch <mitchdoran92@gmail.com>
2020-06-04 21:59:07 +1000

    Added powershot and slingshot mouse abilities. Abilities on Kunkka, uncomment code in custom_hotkeys_players.js to bind them to mouse 1 and mouse 2

--- game/dota_addons/boss_battles/scripts/vscripts/examples/powerShot.lua
+++ game/dota_addons/boss_battles/scripts/vscripts/examples/powerShot.lua
@@ -1,28 +1,30 @@
+--TODO: stop using globals: powerShotTarget amd 
+
 --I'm using this ability twice, one on key press and once on key release
 powerShot = class({})
 
-local tickCount = 0
-local timerInterval = 0.1
-function powerShot:OnSpellStart()
-	print("powerShot:OnSpellStart()")
 
+local powerShotTarget = nil
+local powerShotRadius = nil
+
+local timerInterval = 0.1
+local powerShotPressed
+function powerShot:OnSpellStart()
 	local origin = self:GetCaster():GetAbsOrigin()
-	print("origin = ", origin)
 
 	--powerShotPressed pressed. Ability used first time
-	if _G.powerShotPressed == nil then 
-		print("_G.powerShotPressed == nil")
-		--this timer will continue to run, so the code inside may get executed even if the above if statement is false
-		_G.powerShotPressed = true -- this flag will signify the next time this code runs it's the second time running.
+	if powerShotPressed == nil then 
+		powerShotPressed = true -- this flag will signify the next time this code runs it's the second time running.
 
+		local tickCount = 0	
 		Timers:CreateTimer(function()  
 			tickCount = tickCount +1
 			origin = self:GetCaster():GetAbsOrigin()
-			local length = tickCount * 13
-			local radius = tickCount * 2
+			local length = 300 + (tickCount * 20)
+			local radius = tickCount * 3
 
-			if _G.powerShotPressed == nil then
-				print("stopping the timer")
+			--stop the timer and reset tickCount
+			if powerShotPressed == nil then
 				tickCount = 0
 				return false -- stop the timer
 			end
@@ -31,8 +33,8 @@ function powerShot:OnSpellStart()
 			DebugDrawLine(origin, endPoint, 0,255,0, true, timerInterval) --green line 
 			DebugDrawCircle(origin, Vector(0,255,0), 128, radius, true, timerInterval) --green circle
 
-			_G.powerShotTarget = endPoint
-			_G.powerShotRadius = radius
+			powerShotTarget = endPoint
+			powerShotRadius = radius
 			return timerInterval
 		end)
 
@@ -40,23 +42,27 @@ function powerShot:OnSpellStart()
 	end
 
 	--powerShotPressed released. Ability used second time
-	if _G.powerShotPressed == true then 
-		print("_G.powerShotPressed == true")
-		_G.powerShotPressed = nil
-		local vec_distance =  _G.powerShotTarget - origin
+	if powerShotPressed == true then 
+		print("powerShotPressed == true")
+		powerShotPressed = nil
+		local vec_distance =  powerShotTarget - origin
 		local direction = (vec_distance):Normalized()
-
-		local speed = 25
+		local speed = 15
 
 		local frameCount = 1
 		Timers:CreateTimer(function()  
+			speed = speed + 1
 			origin = self:GetCaster():GetAbsOrigin()
 			local point = origin + ( frameCount * ( direction * speed ) )
-			DebugDrawCircle(point, Vector(0,255,0), 128, _G.powerShotRadius, true, timerInterval) --green circle
+			DebugDrawCircle(point, Vector(0,255,0), 128, powerShotRadius, true, timerInterval) --green circle
 			
-			--TODO: collision check..
-			--calc distance between point and target.. 
-
+			-- collision check..
+			local dist =  (powerShotTarget - point):Length2D()
+			if dist < 60 then
+				DebugDrawCircle(powerShotTarget, Vector(255,0,0), 255, powerShotRadius, true, timerInterval)
+				return
+			end
+			
 			frameCount = frameCount +1
 			return timerInterval
 		end)
