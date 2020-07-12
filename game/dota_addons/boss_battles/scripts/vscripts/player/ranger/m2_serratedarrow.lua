25185121711d6cba0088acfd48bb51420407c13f
Stefan <stefan.scorse.92@gmail.com>
2020-07-12 22:40:48 +1000

    m2 serrated arrow working will WIP modifier.

---
+++ game/dota_addons/boss_battles/scripts/vscripts/player/ranger/m2_serratedarrow.lua
@@ -0,0 +1,115 @@
+m2_serratedarrow = class({})
+LinkLuaModifier("m2_serratedarrow_modifier", "player/ranger/modifiers/m2_serratedarrow_modifier", LUA_MODIFIER_MOTION_NONE)
+
+function m2_serratedarrow:OnAbilityPhaseStart()
+    if IsServer() then
+
+        self.caster = self:GetCaster()
+
+        EmitSoundOnLocationForAllies(self:GetCaster():GetAbsOrigin(), "Ability.PowershotPull", self:GetCaster())
+
+        self.nfx = ParticleManager:CreateParticle("particles/econ/items/windrunner/windrunner_ti6/windrunner_spell_powershot_channel_ti6.vpcf", PATTACH_POINT, self.caster)
+        ParticleManager:SetParticleControlEnt(self.nfx, 0, self.caster, PATTACH_POINT_FOLLOW, "bow_mid1", self.caster:GetAbsOrigin(), true)
+        ParticleManager:SetParticleControl(self.nfx, 1, self.caster:GetAbsOrigin())
+        ParticleManager:SetParticleControlForward(self.nfx, 1, self.caster:GetForwardVector())
+
+        -- start casting animation
+        -- the 1 below is imporant if set incorrectly the animation will stutter (second variable in startgesture is the playback override)
+        self:GetCaster():StartGestureWithPlaybackRate(ACT_DOTA_CAST_ABILITY_2, 0.8)
+
+        -- add casting modifier
+        self:GetCaster():AddNewModifier(self:GetCaster(), self, "casting_modifier_thinker",
+        {
+            duration = self:GetCastPoint(),
+            bMovementLock = true,
+        })
+
+        return true
+    end
+end
+---------------------------------------------------------------------------
+
+function m2_serratedarrow:OnAbilityPhaseInterrupted()
+    if IsServer() then
+
+        -- remove casting animation
+        self:GetCaster():FadeGesture(ACT_DOTA_CAST_ABILITY_2)
+
+        -- remove casting modifier
+        self:GetCaster():RemoveModifierByName("casting_modifier_thinker")
+
+        -- destroy channel particle
+        ParticleManager:DestroyParticle( self.nfx, true )
+
+    end
+end
+---------------------------------------------------------------------------
+
+function m2_serratedarrow:OnSpellStart()
+    if IsServer() then
+
+        -- destroy channel particle
+        ParticleManager:DestroyParticle( self.nfx, true )
+
+        -- init
+		self.caster = self:GetCaster()
+        local origin = self.caster:GetAbsOrigin()
+        local projectile_speed = self:GetSpecialValueFor( "proj_speed" )
+
+        -- play sound
+        EmitSoundOn("Ability.Powershot", self.caster)
+
+        -- set proj direction to mouse location
+        local vTargetPos = nil
+        vTargetPos = GameMode.mouse_positions[self.caster:GetPlayerID()]
+        local projectile_direction = (Vector( vTargetPos.x - origin.x, vTargetPos.y - origin.y, 0 )):Normalized()
+
+        local projectile = {
+            EffectName = "particles/units/heroes/hero_windrunner/windrunner_spell_powershot.vpcf",--"particles/ranger/m1_ranger_atk3_windrunner_base_attack.vpcf",
+            vSpawnOrigin = origin + Vector(0, 0, 100),
+            fDistance = self:GetCastRange(Vector(0,0,0), nil),
+            fUniqueRadius = self:GetSpecialValueFor( "hit_box" ),
+            Source = self.caster,
+            vVelocity = projectile_direction * projectile_speed,
+            UnitBehavior = PROJECTILES_DESTROY,
+            TreeBehavior = PROJECTILES_DESTROY,
+            WallBehavior = PROJECTILES_DESTROY,
+            GroundBehavior = PROJECTILES_NOTHING,
+            fGroundOffset = 80,
+            UnitTest = function(_self, unit)
+                return unit:GetTeamNumber() ~= self.caster:GetTeamNumber()
+            end,
+            OnUnitHit = function(_self, unit)
+
+                -- init icelance dmg table
+                local dmgTable = {
+                    victim = unit,
+                    attacker = self.caster,
+                    damage = self:GetSpecialValueFor( "dmg" ),
+                    damage_type = self:GetAbilityDamageType(),
+                }
+
+                -- give mana
+                self.caster:ManaOnHit(self:GetSpecialValueFor( "mana_gain_percent"))
+
+                -- applys base dmg icelance regardles of stacks
+                ApplyDamage(dmgTable)
+
+            end,
+            OnFinish = function(_self, pos)
+                -- add projectile explode particle effect here on the pos it finishes at
+                local particle_cast = "particles/units/heroes/hero_crystalmaiden/maiden_base_attack_explosion.vpcf"
+                local effect_cast = ParticleManager:CreateParticle(particle_cast, PATTACH_WORLDORIGIN, nil)
+                ParticleManager:SetParticleControl(effect_cast, 0, pos)
+                ParticleManager:SetParticleControl(effect_cast, 3, pos)
+                ParticleManager:ReleaseParticleIndex(effect_cast)
+
+                -- maybe add small ground aoe here as well if we decide icelance does a sml aoe for dmg
+            end,
+        }
+
+        -- create projectile and launch it
+        Projectiles:CreateProjectile(projectile)
+	end
+end
+----------------------------------------------------------------------------------------------------------------
