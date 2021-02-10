6ec9be59d1f240f860631f0c3a8efa0865c2e6ab
Stefan <stefan.scorse.92@gmail.com>
2021-02-10 22:07:01 +1100

    removed the not working cast point override

--- game/dota_addons/boss_battles/scripts/vscripts/core/ability_override.lua
+++
@@ -1,8 +0,0 @@
-function CDOTABaseAbility:GetCastPoint()
-
-    print("running cast point override")
-    -- whirling winds handler
-    if self:GetCaster():HasModifier("e_whirling_winds_modifier") and self:GetAbilityIndex() == 0 then
-        return self:GetCastPoint() - ( self:GetCastPoint() * flWHIRLING_WINDS_CAST_POINT_REDUCTION )
-    end
-end
