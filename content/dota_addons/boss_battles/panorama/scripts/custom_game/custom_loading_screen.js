fe5317d3e188056af039e7cd85970ad30d5a4af3
Stefan <stefan.scorse.92@gmail.com>
2021-03-17 22:30:46 +1100

    loading screen leaderboard init

--- content/dota_addons/boss_battles/panorama/scripts/custom_game/custom_loading_screen.js
+++ content/dota_addons/boss_battles/panorama/scripts/custom_game/custom_loading_screen.js
@@ -2,9 +2,16 @@
 
 (function () {
 
+    $.Msg("hello is this js loading")
+
     // load the header snippet
-    var leaderboardContainer = $("#LeaderboardContainer");
-    var leader_board_header_info = $.CreatePanel("Panel", leaderboardContainer, null);
+    var leaderboardContainer_header = $("#leaderboard_titles");
+    var leader_board_header_info = $.CreatePanel("Panel", leaderboardContainer_header, "");
 	leader_board_header_info.BLoadLayoutSnippet("leaderboard_header_info");	
+
+    // load row snippet
+    var leaderboardContainer_row = $("#leaderboard_rows");
+    var leader_board_row_info = $.CreatePanel("Panel", leaderboardContainer_row, "");
+	leader_board_row_info.BLoadLayoutSnippet("leaderboard_row_info");	
 
 })();
